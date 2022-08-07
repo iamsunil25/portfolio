6d121b485b886efc946348e1ee5980735679a1b2
Sunil Joshi <suniljoshi2621@gmail.com>
2022-08-07 14:45:19 +0530

    contact icons and new tech icons updated

--- portfolio.js
+++ portfolio.js
@@ -4,7 +4,7 @@ let introducImage = document.getElementById("introducImage");
 const height = introducImage.clientHeight;
 const width = introducImage.clientWidth;
 if (!window.screen.width < 700) {
-    document.querySelector("body").style.overflowX = "hidden";
+  document.querySelector("body").style.overflowX = "hidden";
 }
 //getting all sections
 const sections = document.querySelectorAll("section");
@@ -14,169 +14,148 @@ const navlist = document.querySelectorAll("nav ul li");
 
 // tab active with scrolling according to section
 function sectionActive() {
-    let current = "";
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if (pageYOffset >= sectionTop - sectionHeight / 3) {
-            current = section.getAttribute("id");
-        }
-    });
-    navlist.forEach((li) => {
-        li.classList.remove("active");
-        if (li.classList.contains(current)) {
-            li.classList.add("active");
-        }
-    });
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+  navlist.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
 }
 
 // this function is used for handling mobile responsiveness
 function handleMobileResponsiveness() {
-    if (window.screen.width < 700) {
-        document.getElementById("line").classList.remove("line");
-        document.getElementById("line2").classList.remove("line2");
-        document.getElementById("line3").classList.remove("line");
-        document.getElementById("vertical").classList.remove("vertical");
-        document.getElementById("circle1").classList.remove("circle1");
-        document.getElementById("circle2").classList.remove("circle1");
-        document.getElementById("circle3").classList.remove("circle1");
-        document.getElementById("about1").classList.remove("mt-5", "mb-5");
-        document.getElementById("about2").classList.remove("mb-5");
-        document.getElementById("about3").classList.remove("mt-5");
-        document.getElementById("about3").classList.add("mt-4");
-        document.getElementById("about2").classList.add("mt-4");
-        document.getElementById("home").style.marginTop = "20%";
-        document.querySelector("main").style.marginTop = "0px";
-        document.getElementById("navbar").style.display = "none";
-        document.getElementById("myEmail").style.fontSize = "1.4rem";
-        document.getElementById("myHometown").style.fontSize = "1.4rem";
-        document.getElementById("contactheader").style.marginLeft = "-15px";
-        document.getElementById("crossIcon").style.display="none"
-//         if(document.getElementById("navbar"))
-// {
+  if (window.screen.width < 700) {
+    document.getElementById("line").classList.remove("line");
+    document.getElementById("line2").classList.remove("line2");
+    document.getElementById("line3").classList.remove("line");
+    document.getElementById("vertical").classList.remove("vertical");
+    document.getElementById("circle1").classList.remove("circle1");
+    document.getElementById("circle2").classList.remove("circle1");
+    document.getElementById("circle3").classList.remove("circle1");
+    document.getElementById("about1").classList.remove("mt-5", "mb-5");
+    document.getElementById("about2").classList.remove("mb-5");
+    document.getElementById("about3").classList.remove("mt-5");
+    document.getElementById("about3").classList.add("mt-4");
+    document.getElementById("about2").classList.add("mt-4");
+    document.getElementById("home").style.marginTop = "20%";
+    document.querySelector("main").style.marginTop = "0px";
+    document.getElementById("navbar").style.display = "none";
+    document.getElementById("myEmail").style.fontSize = "1.4rem";
+    document.getElementById("myHometown").style.fontSize = "1.4rem";
+    document.getElementById("contactheader").style.marginLeft = "-15px";
+    document.getElementById("crossIcon").style.display = "none";
 
-//   let navbar = document.getElementById("navbar").style
-//   navbar.display="flex";
-//   navbar.position="fixed"
-//   console.log(document.getElementsByTagName("li").classList);
-// }       
-    }
+  }
 
-    if (window.screen.width > 800) {
-        hamburgericon.parentElement.style.display = "none";
-        document.getElementById("about1").classList.add("mt-5", "mb-5");
-        document.getElementById("about2").classList.add("mb-5");
-        document.getElementById("about3").classList.add("mt-5");
-    }
+  if (window.screen.width > 800) {
+    hamburgericon.parentElement.style.display = "none";
+    document.getElementById("about1").classList.add("mt-5", "mb-5");
+    document.getElementById("about2").classList.add("mb-5");
+    document.getElementById("about3").classList.add("mt-5");
+  }
 }
 handleMobileResponsiveness();
 
-//handline for hover effect on image 
+//handling  hover effect on image
 introducImage.addEventListener("mousemove", handleMove);
 
 function handleMove(e) {
-    const xVal = e.layerX;
-    const yVal = e.layerY;
+  const xVal = e.layerX;
+  const yVal = e.layerY;
 
-    const yRotation = 20 * ((xVal - width / 2) / width);
-    const xRotation = -20 * ((yVal - height / 2) / height);
+  const yRotation = 20 * ((xVal - width / 2) / width);
+  const xRotation = -20 * ((yVal - height / 2) / height);
 
-    const string =
-        "perspective(500px) scale(1.1) rotateX(" +
-        xRotation +
-        "deg) rotateY(" +
-        yRotation +
-        "deg)";
+  const string =
+    "perspective(500px) scale(1.1) rotateX(" +
+    xRotation +
+    "deg) rotateY(" +
+    yRotation +
+    "deg)";
 
-    introducImage.style.transform = string;
+  introducImage.style.transform = string;
 }
 
-introducImage.addEventListener("mouseout", function() {
-    introducImage.style.transform =
-        "perspective(500px) scale(1) rotateX(0) rotateY(0)";
+introducImage.addEventListener("mouseout", function () {
+  introducImage.style.transform =
+    "perspective(500px) scale(1) rotateX(0) rotateY(0)";
 });
 
-introducImage.addEventListener("mousedown", function() {
-    introducImage.style.transform =
-        "perspective(500px) scale(0.9) rotateX(0) rotateY(0)";
+introducImage.addEventListener("mousedown", function () {
+  introducImage.style.transform =
+    "perspective(500px) scale(0.9) rotateX(0) rotateY(0)";
 });
 
-introducImage.addEventListener("mouseup", function() {
-    introducImage.style.transform =
-        "perspective(500px) scale(1.1) rotateX(0) rotateY(0)";
+introducImage.addEventListener("mouseup", function () {
+  introducImage.style.transform =
+    "perspective(500px) scale(1.1) rotateX(0) rotateY(0)";
 });
 
 //this is used for show navbar when scrolltop >200
 function navbarDisplay() {
-    if (document.documentElement.scrollTop > 200 && window.screen.width > 700) {
-        navbar.style.display = "block";
-
-    }
-     else if (window.screen.width > 700) {
-             navbar.style.display = "none";
-        
-    }
+  if (document.documentElement.scrollTop > 200 && window.screen.width > 700) {
+    navbar.style.display = "block";
+  } else if (window.screen.width > 700) {
+    navbar.style.display = "none";
+  }
 }
 
 //this is  used for show arrow icon when scrolltop >300
 function upArrowDispaly() {
-    let upArrowIcon = document.getElementById("upArrow");
-    if (document.documentElement.scrollTop > 500) {
-        upArrowIcon.style.display = "block";
-        upArrowIcon.children[0].style.display = "inline-block";
-    } else {
-        upArrowIcon.style.display = "none";
-        upArrowIcon.children[0].style.display = "none";
-    }
+  let upArrowIcon = document.getElementById("upArrow");
+  if (document.documentElement.scrollTop > 500) {
+    upArrowIcon.style.display = "block";
+    upArrowIcon.children[0].style.display = "inline-block";
+  } else {
+    upArrowIcon.style.display = "none";
+    upArrowIcon.children[0].style.display = "none";
+  }
 }
 
 // detecting scrolling
-window.onscroll = function() {
-    sectionActive();
-    navbarDisplay();
-    upArrowDispaly();
+window.onscroll = function () {
+  sectionActive();
+  navbarDisplay();
+  upArrowDispaly();
 };
 
 // for hiding navbar
 let crossIcon = document.getElementById("crossIcon");
 crossIcon.addEventListener("click", () => {
-    navbar.style.display = "none";
+  navbar.style.display = "none";
 });
 
 // toggling sidebar
 hamburgericon.addEventListener("click", () => {
-    if (navbar.style.display == "none") {
-        // let navbar = document.getElementById("navbar").style
-        navbar.style.display="block"
-        navbar.style.position="fixed"
-let body = document.querySelector("body")
-if(body && body.classList){
-body.classList.add("stop-scrolling")
-}
-
+  if (navbar.style.display == "none") {
+    // let navbar = document.getElementById("navbar").style
+    navbar.style.display = "block";
+    navbar.style.position = "fixed";
+    let body = document.querySelector("body");
+    if (body && body.classList) {
+      body.classList.add("stop-scrolling");
     }
-     else {
-        navbar.style.display = "none";
-    }
+  } else {
+    navbar.style.display = "none";
+  }
 });
 
 //for hiding sidebar
-
 const navlist2 = document.querySelectorAll("nav ul li a");
 navlist2.forEach((li) => {
-    li.addEventListener("click",()=>{
-        navbar.style.display="none";
-        let body = document.querySelector("body")
-if(body && body.classList){
-body.classList.remove("stop-scrolling")
-}
-    })
+  li.addEventListener("click", () => {
+    navbar.style.display = "none";
+    let body = document.querySelector("body");
+    if (body && body.classList) {
+      body.classList.remove("stop-scrolling");
+    }
+  });
 });
-// document.addEventListener("click", (event) => {
-//     const element = document.getElementById("navbar");
-//     const closest = event.target.closest(`.${element.id}`);
-//     console.log(closest);
-//     if (!closest && event.target.id != "hamburgericon") {
-//         navbar.style.display = "none";
-//     }
-// });
